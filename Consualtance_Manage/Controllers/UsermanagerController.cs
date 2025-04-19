8a7e5e6357ea9f7c0e6bcdc4b826703a53c71493
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2025-04-19 21:48:20 +0530

    Cretae doctor part

---
+++ Consualtance_Manage/Controllers/UsermanagerController.cs
@@ -0,0 +1,66 @@
+﻿using Consualtance_Manage.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Consualtance_Manage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsermanagerController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public UsermanagerController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("createDoctor")]
+        public async Task<ActionResult> createDoctor(string email)
+        {
+            try
+            {
+                var createDoctor = await _context.Users.FirstOrDefaultAsync(x => x.Email == email);
+                if (createDoctor == null)
+                {
+                    return NotFound("User not found");
+                }
+
+                createDoctor.RoleManager = "Doctor";
+
+                _context.Users.Update(createDoctor);
+                await _context.SaveChangesAsync();
+                return Ok("Doctor created successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpPost("CreateAdmin")]
+        public async Task<ActionResult> CreateAdmin(string email)
+        {
+            try
+            {
+                var createAdmin = await _context.Users.FirstOrDefaultAsync(x => x.Email == email);
+                if (createAdmin == null)
+                {
+                    return NotFound("User not found");
+                }
+
+                createAdmin.RoleManager = "Admin";
+
+                _context.Users.Update(createAdmin);
+                await _context.SaveChangesAsync();
+                return Ok("Admin created successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+
+     }
+}
