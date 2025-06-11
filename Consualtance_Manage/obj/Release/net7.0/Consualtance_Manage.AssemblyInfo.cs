0c9df0f596bc533e9b723b0ee7ab3613eaefa047
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2025-06-11 19:43:40 +0530

    fix

--- Consualtance_Manage/obj/Release/net7.0/Consualtance_Manage.AssemblyInfo.cs
+++ Consualtance_Manage/obj/Release/net7.0/Consualtance_Manage.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consualtance_Manage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1875ff573b9c693e0fb04d46244e0f80835c0d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0799b5a1bd7a6c6415082ff8ec169e039e575a3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consualtance_Manage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consualtance_Manage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
